8dfc19e4cf8761bbeba9b768707a79e65570977a
chloejones9464 <jones.chloe@live.co.uk>
2025-06-25 16:24:45 +0100

    Add Event model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, 'Draft'), (1, 'Published'))
 
 # Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    date = models.DateTimeField()
+    location = models.CharField(max_length=200)
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.IntegerField(choices=STATUS, default=0)  
+
+    
