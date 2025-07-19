0b10e30f7b816fa78fa2df5d54c2f8605b8d1108
chloejones9464 <jones.chloe@live.co.uk>
2025-07-19 14:20:43 +0100

    add style to events pages and fix status bug

--- events/forms.py
+++ events/forms.py
@@ -14,6 +14,7 @@ class EventForm(forms.ModelForm):
             'description',
             'date',
             'location',
+            'status',
         ]
         widgets = {
             'description': SummernoteWidget(),
