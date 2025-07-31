fd5c66c6d1ddd6ad6756ee97c790766899ba5bbc
chloejones9464 <jones.chloe@live.co.uk>
2025-07-31 13:01:21 +0100

    Add whitenoise storage to settings

--- whats_on/settings.py
+++ whats_on/settings.py
@@ -173,6 +173,7 @@ ACCOUNT_FORMS = {
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 SUMMERNOTE_CONFIG = {
     'iframe': True,
